c6ed7fe7384810576072ff27bf39146da18f3208
John O'Neill <john.oneill@alforis.com>
2023-03-14 11:32:55 +0000

    14032023

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "mysite/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "mysite/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
