b52181c8dfccc7ddb330cece15b1dc7f1951f2d1
John O'Neill <john.oneill@alforis.com>
2023-03-15 23:02:39 +0000

    15032023

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,7 +16,7 @@ def index(request):
     )
 
     sql = """SELECT source, publishedDate, dateAdded, link, linkHash 
-            FROM thesis_vert.polls_trainingCorpus
+            FROM polls_trainingCorpus
             ORDER BY dateAdded;"""
 
     mycursor = mydb.cursor()
