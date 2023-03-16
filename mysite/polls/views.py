c8d12d7bb76b1d8b2eed4aa29518f082c06c3503
John O'Neill <john.oneill@alforis.com>
2023-03-16 11:44:23 +0000

    16032023

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,41 +2,41 @@ from django.shortcuts import render
 from datetime import date
 from django.template import loader
 from django.http import HttpResponse
+from .models import trainingCorpus
 
 import mysql.connector
 import os
 
-data_dict = {}
+# data_dict = {}
 
 
 def index(request):
     # Database Connection to Pull Existing Meta Values
-    mydb = mysql.connector.connect(
-        user=(os.getenv("db_user")),
-        password=(os.getenv("db_password")),
-        host="localhost",
-        database="thesis_vert",
-    )
+    # mydb = mysql.connector.connect(
+    #    user=(os.getenv("db_user")),
+    #    password=(os.getenv("db_password")),
+    #    host="localhost",
+    #    database="thesis_vert",
+    # )
 
-    sql = """SELECT source, publishedDate, dateAdded, link, linkHash 
-            FROM polls_trainingcorpus
-            ORDER BY dateAdded;"""
+    # sql = """SELECT source, publishedDate, dateAdded, link, linkHash
+    #        FROM polls_trainingcorpus
+    #        ORDER BY dateAdded;"""
 
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
+    # mycursor = mydb.cursor()
+    # mycursor.execute(sql)
 
-    records = mycursor.fetchall()
+    # records = mycursor.fetchall()
 
     # return HttpResponse(records)
 
-    thisdict = {
-        "brand": "Ford",
-        "model": "Mustang",
-        "year": 1964,
-        "brand": "Nissan",
-        "model": "Almera",
-        "year": 2002,
+    corpusData = trainingCorpus.objects.all()
+
+    context = {
+        "entries": corpusData,
     }
 
+    # return HttpResponse(corpusData)
+
     template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render(thisdict, request))
+    return HttpResponse(template.render(context, request))
