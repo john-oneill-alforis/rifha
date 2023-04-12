d8b999ec5af5fff4cb8f96ee8cd8b5b2f5ce9d48
John O'Neill <john.oneill@alforis.com>
2023-04-12 22:53:46 +0100

    12-04-2023

--- scripts/content-retrieve-verisdb.py
+++ scripts/content-retrieve-verisdb.py
@@ -4,15 +4,20 @@ from os.path import exists
 
 
 def sync_veris():
-    cdir = os.getcwd()
-    print("Current Directory: ", cdir)
-    print("Parent Directory: ", os.path.dirname(cdir))
+    try:
+        cdir = os.getcwd()
+        # print("Current Directory: ", cdir)
+        # print("Parent Directory: ", os.path.dirname(cdir))
 
-    os.path.exists("veris-data/.git")
+        os.path.exists(cdir + "veris-data/.git")
 
-    repo = Repo("veris-data")
-    origin = repo.remote(name="origin")
-    origin.pull()
+        repo = Repo("veris-data")
+        origin = repo.remote(name="origin")
+        origin.pull()
+
+    except:
+        cdir = os.getcwd()
+        print("Current Directory: ", cdir)
 
 
 sync_veris()
