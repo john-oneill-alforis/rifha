7cfcfcf0c7c904068c664903c9738e4f1bbe3cd5
John O'Neill <john.oneill@alforis.com>
2023-04-12 22:05:50 +0100

    12-04-2023

--- scripts/content-retrieve-verisdb.py
+++ scripts/content-retrieve-verisdb.py
@@ -6,6 +6,7 @@ from os.path import exists
 def sync_veris():
     try:
         os.path.exists("veris-data/.git")
+
         repo = Repo("veris-data")
 
         origin = repo.remote(name="origin")
@@ -13,6 +14,8 @@ def sync_veris():
 
     except:
         print("Repo Does Not Exsist")
+        current_working_directory = os.getcwd()
+        print(current_working_directory)
 
 
 sync_veris()
