752f0d45286d92e8dd1b6ffdac62f71c24c8b5a9
John O'Neill <john.oneill@alforis.com>
2023-04-22 17:16:35 +0100

    22-04-2023

--- scripts/data-parser-verisdb.py
+++ scripts/data-parser-verisdb.py
@@ -15,7 +15,6 @@ def process_veris_information():
     load_dotenv()
     # Load up the database
     try:
-        # engine = db.create_engine(os.environ.get("db_connection_string"), echo=True)
         engine = db.create_engine(
             "mysql+mysqldb://"
             + (os.getenv("db_user"))
@@ -25,7 +24,11 @@ def process_veris_information():
         )
         conn = engine.connect()
         metadata = db.MetaData()
-        veris = db.Table("veris_test", metadata, autoload_with=engine)
+
+        # Load the Database Tables we will need for the insertion
+        veris = db.Table("polls_veris_incident_details", metadata, autoload_with=engine)
+        veris = metadata.tables["veris_test"]
+
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -35,17 +38,23 @@ def process_veris_information():
     # level as the scripts folder
     toplevel_path = Path(__file__).parents[1]
 
+    ############################################################
     # Concat paths for both the submitted and validated folders
-    # veris_submitted_data = toplevel_path / "veris-data/data/json/submitted"
+    ############################################################
     veris_validated_data = toplevel_path / "veris-data/data/json/validated"
 
     # Grab the data from the validated reports
+
+    incident_id = ""
     for p in veris_validated_data.rglob("*.json"):
         # Load the data from the json file
         json_file = veris_validated_data / p.name
 
         data = json.loads(json_file.read_bytes())
-        veris = metadata.tables["veris_test"]
+
+        ########################################################
+        # Code block to write the initial incident data
+        ########################################################
 
         try:
             query = (
