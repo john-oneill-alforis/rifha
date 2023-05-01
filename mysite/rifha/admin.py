5aaba064a8556b677139687ed901e14581576bae
John O'Neill <john.oneill@alforis.com>
2023-05-01 21:33:47 +0100

    01-05-2023

--- mysite/rifha/admin.py
+++ mysite/rifha/admin.py
@@ -4,8 +4,15 @@ from django.contrib import admin
 
 from .models import staff
 from .models import staffRole
+from .models import assets
+from .models import assetsTypes
+from .models import processes
+
 
 # Register your models here.
 
 admin.site.register(staff)
 admin.site.register(staffRole)
+admin.site.register(assets)
+admin.site.register(assetsTypes)
+admin.site.register(processes)
