e7d0a775a43c499dd4470948f0cabc1039fc1e17
John O'Neill <john.oneill@alforis.com>
2023-05-01 16:49:25 +0100

    01-05-2023

--- mysite/rifha/admin.py
+++ mysite/rifha/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import staff
+from .models import staffRole
+
+# Register your models here.
+
+
+admin.site.register(staff)
+admin.site.register(staffRole)
--- mysite/rifha/models.py
+++ mysite/rifha/models.py
@@ -1,18 +1,27 @@
 from django.db import models
+
 import uuid
 import datetime
 
 # Create your models here.
 
 
-class businessProcesses(models.Model):
-    entryId = models.BigAutoField(primary_key=True)
-    processId = models.UUIDField()
-    name = models.CharField(max_length=254)
-    description = models.TextField()
-    owner = models.IntegerField()
-    dateCreated = models.DateField()
-    reviewDate = models.DateField()
+class staff(models.Model):
+    staffId = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    contactNUmber = models.CharField(max_length=100)
+    jobTitle = models.ForeignKey("staffRole", on_delete=models.CASCADE, default=1)
+
+    def __unicode__(self):
+        return self.name
+
+
+class staffRole(models.Model):
+    roleId = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4)
+    roleTitle = models.CharField(max_length=100)
+    description = models.CharField(max_length=254)
 
     def __unicode__(self):
         return self.name
