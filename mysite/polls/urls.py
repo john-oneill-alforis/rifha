a1b7d76fbc0903e3ab33ad74fb214f035f10ffbb
John O'Neill <john.oneill@alforis.com>
2023-05-27 22:27:40 +0100

    27-05-2023

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("verisaro", views.verisaro, name="verisaro"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("home/", views.dashboard, name="home"),
 ]
