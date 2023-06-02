147c82a92a475332a0aa9e0472b5296935a64b89
John O'Neill <john.oneill@alforis.com>
2023-06-02 22:02:10 +0100

    02062023

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -2,14 +2,14 @@ from django import forms
 
 
 class interviewForm(forms.Form):
-    subject_uuid = forms.CharField(label="Subject UUID", max_length=36),
-    question_1 = forms.CharField(widget=forms.Textarea label="Question 1"),
-    question_2 = forms.CharField(label="Question 2"),
-    question_3 = forms.CharField(label="Question 3"),
-    question_4 = forms.CharField(label="Question 4"),
-    question_5 = forms.CharField(label="Question 5"),
-    question_6 = forms.CharField(label="Question 6"),
-    question_7 = forms.CharField(label="Question 7"),
-    question_8 = forms.CharField(label="Question 8"),
-    question_9 = forms.CharField(label="Question 9"),
-    question_10 = forms.CharField(label="Question 10")
+    subject_uuid = (forms.CharField(label="Subject UUID", max_length=36),)
+    question_1 = forms.CharField(widget=forms.Textarea)
+    question_2 = forms.CharField(widget=forms.Textarea)
+    question_3 = forms.CharField(widget=forms.Textarea)
+    question_4 = forms.CharField(widget=forms.Textarea)
+    question_5 = forms.CharField(widget=forms.Textarea)
+    question_6 = forms.CharField(widget=forms.Textarea)
+    question_7 = forms.CharField(widget=forms.Textarea)
+    question_8 = forms.CharField(widget=forms.Textarea)
+    question_9 = forms.CharField(widget=forms.Textarea)
+    question_10 = forms.CharField(widget=forms.Textarea)
