bc5dbf51c226384bcdbe03fc354ebe7c2a127449
John O'Neill <john.oneill@alforis.com>
2023-06-14 15:17:38 +0100

    14-06-2023

--- mysite/rifha/forms.py
+++ mysite/rifha/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from .models import staff
-from .models import assetsClassifications
+from .models import assetsClassifications, assetsTypes, assets, staff
 
 
 class peopleAddForm(forms.Form):
@@ -42,3 +41,48 @@ class classificationEditForm(forms.ModelForm):
             "classificationLabel": forms.TextInput(),
             "classificationDescription": forms.Textarea,
         }
+
+
+class assettTypeAddForm(forms.Form):
+    assetTypeLabel = forms.CharField(widget=forms.TextInput, label="Asset Type Label")
+    assettTypeDescription = forms.CharField(
+        widget=forms.Textarea, label="Asset Type Description"
+    )
+
+
+class assetTypesEditForm(forms.ModelForm):
+    class Meta:
+        model = assetsTypes
+        fields = ["assetTypeName", "assetTypeDescription"]
+        widgets = {
+            "assetTypeName": forms.TextInput(),
+            "assetTypeDescription": forms.Textarea,
+        }
+
+
+class addAssetForm(forms.ModelForm):
+    assetType = forms.ModelChoiceField(
+        queryset=assetsTypes.objects.all(), label="Asset Type"
+    )
+
+    assetClassificaion = forms.ModelChoiceField(
+        queryset=assetsClassifications.objects.all(), label="Asset Classification"
+    )
+
+    class Meta:
+        model = assets
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["assetType"].widget = forms.Select(
+            choices=assetsTypes.objects.values_list("assetTypeId", "assetTypeName")
+        )
+        self.fields["assetClassification"].widget = forms.Select(
+            choices=assetsClassifications.objects.values_list(
+                "classification_Id", "classificationLabel"
+            )
+        )
+        self.fields["assetOwner"].widget = forms.Select(
+            choices=staff.objects.values_list("staffId", "jobTitle")
+        )
