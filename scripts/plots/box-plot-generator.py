405820ed330e5d52203447460198bfdf40e77b2c
John O'Neill <john.oneill@alforis.com>
2023-07-29 16:05:50 +0100

    29-07-2023

--- scripts/plots/box-plot-generator.py
+++ scripts/plots/box-plot-generator.py
@@ -24,12 +24,6 @@ def box_plot_generate():
     # Path data to save to the plots folder
     toplevel_path = Path(__file__).parents[2]
 
-
-
-
-   
-
-
     # Create a cursor to interact with the database
     cursor = cnx.cursor()
 
@@ -94,9 +88,9 @@ def box_plot_generate():
 
 
    # Create the horizontal box plot
-    plt.figure(figsize=(10, 6))  # Optional: Adjust the figure size as needed
+    plt.figure(figsize=(12, 6))  # Optional: Adjust the figure size as needed
 
-    box_props = dict(facecolor="blue", edgecolor="black")  # Customize box color and edge color
+    box_props = dict(facecolor="orange", edgecolor="black")  # Customize box color and edge color
 
     # Fill the boxes with the specified color
     bplot = plt.boxplot(data_lists, vert=False, labels=labels, patch_artist=True, boxprops=box_props)
@@ -107,12 +101,97 @@ def box_plot_generate():
 
     # Custom legend for the filled boxes
     for patch in bplot['boxes']:
-        patch.set_facecolor("lightblue")
+        patch.set_facecolor("orange")
 
         # Explicitly show the plot
         
     plt.savefig(toplevel_path / "mysite/static/plots/box-question-1.png")
     #plt.show()
+
+
+    ########################################################################
+    # Plots for Question Two 
+    ########################################################################
+
+
+    # Execute the query for the first data series
+    query1 = """SELECT positivity_score FROM polls_transcriptcapture
+                WHERE question_id = 2;"""
+
+    cursor.execute(query1)
+
+    # Fetch the data for the first data series
+    rows1 = cursor.fetchall()
+
+    # Extract the x and y coordinates from the fetched data for the first data series
+    list1 = [row[0] for row in rows1]  # Assuming x values are in the first column
     
+    # Execute the query for the second data series
+    query2 =  """SELECT neutrality_score FROM polls_transcriptcapture
+                WHERE question_id = 2;"""
+
+    cursor.execute(query2)
+
+    # Fetch the data for the second data series
+    rows2 = cursor.fetchall()
+
+    # Extract the x and y coordinates from the fetched data for the second data series
+    list2 = [row[0] for row in rows2]  # Assuming x values are in the first column
+
+
+    # Execute the query for the second data series
+    query3 =  """SELECT negativity_score FROM polls_transcriptcapture
+                WHERE question_id = 2;"""
+
+    cursor.execute(query3)
+
+    # Fetch the data for the second data series
+    rows3 = cursor.fetchall()
+
+    # Extract the x and y coordinates from the fetched data for the second data series
+    list3 = [row[0] for row in rows3]  # Assuming x values are in the first column
+
+
+    # Execute the query for the second data series
+    query4 =  """SELECT compound_score FROM polls_transcriptcapture
+                WHERE question_id = 2;"""
+
+    cursor.execute(query4)
+
+    # Fetch the data for the second data series
+    rows4 = cursor.fetchall()
+
+    # Extract the x and y coordinates from the fetched data for the second data series
+    list4 = [row[0] for row in rows4]  # Assuming x values are in the first column
+
+
+     # Labels for each list
+    labels = ["Positivity Score", "Neutrality Score", "Negativity Score", "Compound Score"]
+
+    # Combine the lists into one list of lists
+    data_lists = [list1, list2, list3, list4]
+
+    print(list1,list2,list3,list4)
+
+
+   # Create the horizontal box plot
+    plt.figure(figsize=(12, 6))  # Optional: Adjust the figure size as needed
+
+    box_props = dict(facecolor="blue", edgecolor="black")  # Customize box color and edge color
+
+    # Fill the boxes with the specified color
+    bplot = plt.boxplot(data_lists, vert=False, labels=labels, patch_artist=True, boxprops=box_props)
+
+    plt.xlabel("Values")
+    plt.ylabel("Lists")
+    plt.title("Interview Question 2 - Response Values")
+
+    # Custom legend for the filled boxes
+    for patch in bplot['boxes']:
+        patch.set_facecolor("blue")
+
+        # Explicitly show the plot
+        
+    plt.savefig(toplevel_path / "mysite/static/plots/box-question-2.png")
 
 box_plot_generate()
